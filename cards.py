28d24e3014f1cf6fd7ef5f2a66002809d3334f32
EmmaPonts024 <emmamc.leblanc@gmail.com>
2025-11-02 18:06:59 +0100

    card class
    defines cards, and sequences

--- cards.py
+++ cards.py
@@ -1,46 +1,58 @@
+
+from enum import Enum
+from typing import List
+
 # Cards : defines the Card class and related functions (enumerate ?) 
 
-class Suit(enum.Enum): #suit means "couleur" in french
+class Suit(Enum): #suit means "couleur" in french
     CLUBS = "clubs"
     DIAMONDS = "diamonds"
     HEARTS = "hearts"
     SPADES = "spades"
     JOKER = "joker"
 
-class Value(enum.Enum):
-    ACE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-
 class Card:
-    def __init__(self, suit: Suit, value:Value):
-        self.suit = suit
+    def __init__(self, suit: Suit, value: int):
+        if not (1 <= value <= 13):
+          raise ValueError("value must be between 1 and 13")
         self.value = value
-    def __lt__(self,other):
-        if self.suit == JOKER or other.suit == JOKER: #(self.suit.name?)
-            return True
-        if self.value == 2 or other.value == 2:
-            return True
-        if self.value <= other.value:
-            return True
-        if self.value == 13 and other.value == 1: #king<=ace
-            return True
-        return False #tous les autres Cards
+        self.suit = suit
 
+    @staticmethod
+    def follows(card1: "Card", card2: "Card") -> bool:
+        """Return True iff card2 comes right after card1."""
+        if card1.suit == "joker" or card2.suit == "joker":
+            return True
+        if card1.value == 2 or card2.value == 2:
+            return True
+        if card1.value == card2.value - 1:
+            return True
+        if card1.value == 13 and card2.value == 1:  # Ace follows King
+            return True
+        return False
 
-a_color = Suit.CLUBS
-a_value = Value.FIVE
-other_value = Value.SIX
-card_1 = Card(a_color,a_value)
-card_2 = Card(a_color,other_value)
-print(card1<=card2)
+    @staticmethod
+    def follows_pure(card1: "Card", card2: "Card") -> bool:
+        """Return True iff card2’s value is exactly one higher than card1’s."""
+        if card1.value == card2.value - 1:
+            return True
+        if card1.value == 13 and card2.value == 1:  # Ace follows King
+            return True
+        return False
+
+    @staticmethod
+    def is_sequence(card_list: List["Card"]) -> bool:
+      nb_cards = len(card_list)
+      for i in range(nb_cards-1):
+        if not Card.follows(card_list[i],card_list[i+1]):
+          return False
+      return True
+
+    @staticmethod
+    def is_pure_sequence(card_list: List["Card"]) -> bool:
+      nb_cards = len(card_list)
+      for i in range(nb_cards-1):
+        if not Card.follows_pure(card_list[i],card_list[i+1]):
+          return False
+      return True
+

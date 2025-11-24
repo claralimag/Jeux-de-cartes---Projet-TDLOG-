e70711f78817c1e229ec312e9a07139c7625785e
Clara Lima Goldenberg <clara.lima-goldenberg@enpc.fr>
2025-11-24 12:22:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import player
 import cards
 import boardplayer
 import boardgame
+from random import randint
 
 def single_player(nom : str) -> None :
 
@@ -9,5 +10,115 @@ def single_player(nom : str) -> None :
     person : player.Player = player.Player(str, [], board_person, 0)
     
 
+def cards_to_add(cards : list[Card], board : BoardPlayer):
+    #look for a jocker:
+    existsjocker = 0
+    n = len(cards)
+    if cards[-1] == Suit.JOCKER:
+        existsjocker += 1  
+    
+    dico = {}
+    for i in in range(len(board.cardgames)):
+        el = board.cardgames[i]
+        last = el[0][-1]
+        first = el[0][0]
+        j = 0 
+        while j<n:
+            if cards[j] - last== 1: 
+                try:
+                    dico[i].append(cards[j])
+                    last = cards[j]
+                    j = 0
+                except:
+                    dico[i] = [cards[j]]
+                    last = cards[j]
+                    j=0
+            
+            if cards[j] - last == 2 and existsjocker>0:
+                try:
+                    dico[i].append(Suit.JOCKER)
+                    existsjocker -= 1
+                    dico[i].append(cards[j])
+                    last = cards[j]
+                    j = 0
+                except:
+                    dico[i] = [Suit.JOCKER] + [cards[j]]
+                    existsjocker -= 1
+                    last = cards[j]
+                    j=0
+                    
+            
+            if begin - cards[j] == 1: 
+                try:
+                    dico[i].append(cards[j])
+                    last = cards[j]
+                    j = 0
+                except:
+                    dico[i] = [cards[j]]
+                    last = cards[j]
+                    j=0
+            
+            if begin - cards[j] == 2 and existsjocker>0:
+                try:
+                    dico[i].append(Suit.JOCKER)
+                    existsjocker -= 1
+                    dico[i].append(cards[j])
+                    last = cards[j]
+                    j = 0
+                except:
+                    dico[i] = [Suit.JOCKER] + [cards[j]]
+                    existsjocker -= 1
+                    last = cards[j]
+                    j=0
+            else: 
+                j=j+1
+                
+    return dico
+                
+def sequences(cards : list[Card])
+    #Assuning cards is ordered
+    cards_by_color = {}
+    
+    for el in list[Card]:
+        try:
+            cards_by_color[el.suit].append(el)
+        except:
+            cards_by_color[el.suit] = [el]
+    
+    n = len(cards_by_color)
+    list = []
+    for key in cards_by_color:
+        sequence = []
+        for el in cards_by_color[key]:
+            sequence
+            
+            
+    
+    
 
+def robot_easy(robot : Player, open : bool, int: whichplayer) -> Card :
+    # input : Player representing the computer
+    # output : Card to trow out in the trash 
+    if open:
+        draw_from_discard(whichplayer)
+        
+    cards_to_add = can_add(robot.cards, robot.board) #dictionnary where key = where you can add a card and dico[key] list of carda you can add
+    sequences = exists_sequence(robot.cards)
+    deal_with_jocker(cards_to_add, sequences)
+    
+    for key in cards_to_add:
+        robot.play_cards(cards_to_add[key], key, False)
+        
+    for el in sequences:
+        robot.play_cards(el,-1)
+    
+    #Throw out a random card:
+    n = len(robot.cards)
+    
+    i = randint(0,n)
+    
+    return robot.cards[i]
+        
+
+    
 
